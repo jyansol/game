f65c776babc33795d2e10bfc2cdddc36fa7ffe84
jyansol <hahn0406@gmail.com>
2018-10-29 09:52:40 +0900

    modified

--- src/index.js
+++ src/index.js
@@ -28,15 +28,16 @@ document.querySelector('.select-section').addEventListener('click', (e) => {
   while (newArr.length > 3) {
     alert('숫자는 3개만 입력가능합니다.');
     newArr.pop();
-    console.log(newArr);
   }
-  if (newArr[0] === newArr[1]) {
-    alert('다른숫자를 입력하세요');
-    newArr.pop();
-    document.querySelector('.list1').textContent = newArr[0];
-    document.querySelector('.list2').textContent = newArr[1];
-    document.querySelector('.list3').textContent = newArr[2];
-  }
+  console.log(newArr);
+
+  // if (newArr[0] === newArr[1] && newArr[1] === newArr[2]) {
+  //   alert('다른숫자를 입력하세요');
+  //   newArr.pop();
+  //   document.querySelector('.list1').textContent = newArr[0];
+  //   document.querySelector('.list2').textContent = newArr[1];
+  //   document.querySelector('.list3').textContent = newArr[2];
+  // }
 });
 
 const viewReset = () => {
@@ -98,39 +99,39 @@ document.querySelector('.pitch').addEventListener('click', (e) => {
 
   // while (count < 10) {
   // 숫자를 입력받고 비교를 준비하는 부분
-  // let input = newArr[]
-  // let inputArray = input.split('');
+  // let input = newArr;
+  // let inputArray = newArr.split('');
   // let inputArray = input.split(""); //input.split(''); // split함수는 아래에 설명
 
   strike = 0; // strike와 ball의 개수를 초기화
   ball = 0;
   count++; // 시도 횟수는 하나 증가
   // 입력받은 숫자를 비교분석하는 부분
-  // for (let j = 0; j < 3; j++) {
-  //   for (let k = 0; k < 3; k++) {
-  //     if (number[j] == inputArray[k]) {
-  //       if (j === k) {
-  //         strike++;
-  //       } else {
-  //         ball++;
-  //       }
-  //     }
-  //   }
-  // }
+  for (let j = 0; j < 3; j++) {
+    for (let k = 0; k < 3; k++) {
+      if (number[j] == newArr[k]) {
+        if (j === k) {
+          strike++;
+        } else {
+          ball++;
+        }
+      }
+    }
+  }
   // 결과를 표시하는 부분 console객체는 밑에 설명
-  // if (strike === 3) {
-  //   console.log('홈런!!! ' + (count - 1) + '번 만에 맞추셨습니다');
-  //   // break;
-  // } else if (count >= 10) {
-  //   console.error('시도 횟수를 초과하셨습니다.');
-  // } else {
-  //   console.info(inputArray.join('') + ': ' + strike + '스트라이크 ' + ball + '볼');
-  //   // document.querySelectorAll('.mystyle')[0].textContent = ''
-  //   // document.querySelectorAll('.mystyle')[1].textContent = ''
-  //   // document.querySelectorAll('.mystyle')[2].textContent = ''
-  //   // inputArray = []
-  //   // inputArray.push('zz')
-  //   // console.log(inputArray)
-  // }
-  // }
+  if (strike === 3) {
+    console.log('홈런!!! ' + (count - 1) + '번 만에 맞추셨습니다');
+    // break;
+  } else if (count >= 10) {
+    console.error('시도 횟수를 초과하셨습니다.');
+  } else {
+    console.info(newArr + ': ' + strike + '스트라이크 ' + ball + '볼');
+    //   // document.querySelectorAll('.mystyle')[0].textContent = ''
+    //   // document.querySelectorAll('.mystyle')[1].textContent = ''
+    //   // document.querySelectorAll('.mystyle')[2].textContent = ''
+    //   // inputArray = []
+    //   // inputArray.push('zz')
+    //   // console.log(inputArray)
+    // }
+  }
 });
